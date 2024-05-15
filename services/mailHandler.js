d9e504e816eb08ce4ac0210834943c750fcbb939
abdul0398 <abdulrkhan570@gmail.com>
2024-05-15 11:47:23 +0530

    Add field option adn email option

--- services/mailHandler.js
+++ services/mailHandler.js
@@ -20,6 +20,7 @@ async function sendMail(lead, receive_email) {
 
     const sender_email = process.env.SENDER_EMAIL;
     const app_pass = process.env.APP_PASS;
+    console.log(sender_email, app_pass, receive_email);
     try {
         const transporter = await initializeNodemailer(sender_email, app_pass);
         const info = await transporter.sendMail({
