6eb0969c058b2612d7d01770b41723d72754e497
abdul0398 <abdulrkhan570@gmail.com>
2024-05-25 15:30:50 +0530

    Feaut: Added download option for leads

--- routes/lead.js
+++ routes/lead.js
@@ -45,6 +45,30 @@ router
         console.error(error);
         next();
     }
+}).post("/api/leads/status", verify, async (req,res)=>{
+    const { ids } = req.body;
+
+    if (!ids || !Array.isArray(ids) || ids.length === 0) {
+        return res.status(400).json({ error: 'Invalid input' });
+    }
+
+    try {
+        // Construct the query to update the status
+        const query = `
+        UPDATE leads
+        SET status = 'JUNK'
+        WHERE id IN (?)
+    `;
+        // Execute the query with the array of ids
+        await __pool.query(query, [ids]);
+
+        // Send a success response
+        res.status(200).json({ message: 'Leads marked as junk successfully' });
+    } catch (error) {
+        console.error('Error marking leads as junk:', error);
+        res.status(500).json({ error: 'An error occurred while updating the leads' });
+    }
+
 })
 
 
