f54985c0b563303cfafa3be3b421b4fc0952b31e
abdul0398 <abdulrkhan570@gmail.com>
2024-09-06 21:24:57 +0530

    feat:created webhook

--- routes/discord.js
+++ routes/discord.js
@@ -87,6 +87,10 @@ router
         console.log("Error getting servers and channels:", error);
         res.status(500).json({message:"Error getting servers and channels"});
     }
+}).post('/api/google-sheet/discord', (req, res)=>{
+    console.log(req.body);
+
+    res.status(200).json({message:"Successfully recieved"})
 })
 
 
